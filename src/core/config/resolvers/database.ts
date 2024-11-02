66cfa445034d27cbaf48a79fb376aef87bcd4f1b
Pooya Parsa <pyapar@gmail.com>
2024-11-02 21:11:27 +0100

    fix(imports): use explicit import for `useDatabase` (#2837)

--- src/core/config/resolvers/database.ts
+++ src/core/config/resolvers/database.ts
@@ -3,7 +3,7 @@ import type { NitroOptions } from "nitropack/types";
 export async function resolveDatabaseOptions(options: NitroOptions) {
   if (options.experimental.database && options.imports) {
     options.imports.presets.push({
-      from: "nitropack/runtime",
+      from: "nitropack/runtime/internal/database",
       imports: ["useDatabase"],
     });
     if (options.dev && !options.database && !options.devDatabase) {
