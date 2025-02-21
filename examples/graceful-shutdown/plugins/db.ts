b0de914ff11991ecc40a35eff33edb4dd25b6cec
@beer <47961062+iiio2@users.noreply.github.com>
2025-02-21 16:21:29 +0600

    chore: fix grammar (#3107)

--- examples/graceful-shutdown/plugins/db.ts
+++ examples/graceful-shutdown/plugins/db.ts
@@ -2,7 +2,7 @@ export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hookOnce("close", async () => {
     console.log("Disconnecting database...");
 
-    // something you want to do, such like disconnected the database, or wait the task is done
+    // something you want to do, such like disconnect the database, or wait until the task is done
     await new Promise((resolve) => setTimeout(resolve, 500));
 
     console.log("Database is disconnected!");
