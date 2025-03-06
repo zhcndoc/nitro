e20071e03786bdeaa2bab01405e0691375af38de
Pooya Parsa <pyapar@gmail.com>
2025-03-06 16:25:43 +0100

    fix(dev): auto fallback to port if socket listening failed (#3165)

--- src/presets/_nitro/runtime/nitro-dev.ts
+++ src/presets/_nitro/runtime/nitro-dev.ts
@@ -22,48 +22,34 @@ const {
   NITRO_DEV_WORKER_ID,
 } = process.env;
 
+// Trap unhandled errors
+trapUnhandledNodeErrors();
+
+// Listen for shutdown signal from runner
+parentPort?.on("message", (msg) => {
+  if (msg && msg.event === "shutdown") {
+    shutdown();
+  }
+});
+
 const nitroApp = useNitroApp();
 
 const server = new Server(toNodeListener(nitroApp.h3App));
+let listener: Server | undefined;
+
+listen()
+  .catch(() => listen(true /* use random port */))
+  // eslint-disable-next-line unicorn/prefer-top-level-await
+  .catch((error) => {
+    console.error("Dev worker failed to listen:", error);
+    return shutdown();
+  });
 
 // https://crossws.unjs.io/adapters/node
 if (import.meta._websocket) {
   const { handleUpgrade } = wsAdapter(nitroApp.h3App.websocket);
   server.on("upgrade", handleUpgrade);
 }
-
-function getAddress() {
-  if (NITRO_NO_UNIX_SOCKET || process.versions.webcontainer) {
-    return 0;
-  }
-
-  const socketName = `worker-${process.pid}-${threadId}-${Math.round(Math.random() * 10_000)}-${NITRO_DEV_WORKER_ID}.sock`;
-  const socketPath = join(NITRO_DEV_WORKER_DIR, socketName);
-
-  switch (process.platform) {
-    case "win32": {
-      return join(String.raw`\\.\pipe\nitro`, socketPath);
-    }
-    case "linux": {
-      return `\0${socketPath}`;
-    }
-    default: {
-      return socketPath;
-    }
-  }
-}
-
-const listenAddress = getAddress();
-const listener = server.listen(listenAddress, () => {
-  const _address = server.address();
-  parentPort?.postMessage({
-    event: "listen",
-    address:
-      typeof _address === "string"
-        ? { socketPath: _address }
-        : { host: "localhost", port: _address?.port },
-  });
-});
 
 // Register tasks handlers
 nitroApp.router.get(
@@ -95,26 +81,58 @@ nitroApp.router.use(
   })
 );
 
-// Trap unhandled errors
-trapUnhandledNodeErrors();
-
-// Force shutdown
-async function onShutdown() {
-  server.closeAllConnections?.();
-  await Promise.all([
-    new Promise((resolve) => listener.close(resolve)),
-    nitroApp.hooks.callHook("close").catch(console.error),
-  ]);
-}
-
-parentPort?.on("message", async (msg) => {
-  if (msg && msg.event === "shutdown") {
-    await onShutdown();
-    parentPort?.postMessage({ event: "exit" });
-  }
-});
-
 // Scheduled tasks
 if (import.meta._tasks) {
   startScheduleRunner();
+}
+
+// --- utils ---
+
+function listen(
+  useRandomPort: boolean = Boolean(
+    NITRO_NO_UNIX_SOCKET || process.versions.webcontainer
+  )
+) {
+  return new Promise<void>((resolve, reject) => {
+    try {
+      listener = server.listen(useRandomPort ? 0 : getSocketAddress(), () => {
+        const address = server.address();
+        parentPort?.postMessage({
+          event: "listen",
+          address:
+            typeof address === "string"
+              ? { socketPath: address }
+              : { host: "localhost", port: address?.port },
+        });
+        resolve();
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+function getSocketAddress() {
+  const socketName = `worker-${process.pid}-${threadId}-${Math.round(Math.random() * 10_000)}-${NITRO_DEV_WORKER_ID}.sock`;
+  const socketPath = join(NITRO_DEV_WORKER_DIR, socketName);
+  switch (process.platform) {
+    case "win32": {
+      return join(String.raw`\\.\pipe\nitro`, socketPath);
+    }
+    case "linux": {
+      return `\0${socketPath}`;
+    }
+    default: {
+      return socketPath;
+    }
+  }
+}
+
+async function shutdown() {
+  server.closeAllConnections?.();
+  await Promise.all([
+    new Promise((resolve) => listener?.close(resolve)),
+    nitroApp.hooks.callHook("close").catch(console.error),
+  ]);
+  parentPort?.postMessage({ event: "exit" });
 }
