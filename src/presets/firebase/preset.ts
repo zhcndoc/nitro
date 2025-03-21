3845524bbf50bc9ce43b914fbb1e9a10d6294046
Yuangwang <yuangwang2@gmail.com>
2025-03-21 07:14:52 -0400

    refactor!: remove firebase functions in favor of firebase app hosting (#3215)
    Co-authored-by: Pooya Parsa <pooya@pi0.io>

--- src/presets/firebase/preset.ts
+++ src/presets/firebase/preset.ts
@@ -1,16 +1,9 @@
 import { defineNitroPreset } from "../_utils/preset";
 import { writeFile } from "../_utils/fs";
 import { version as nitroVersion } from "nitro/meta";
-import { basename, join, relative } from "pathe";
-import type { Plugin } from "rollup";
-import { genSafeVariableName } from "knitwork";
+import { join, relative } from "pathe";
 import { stringifyYAML } from "confbox";
-import { updatePackageJSON, writeFirebaseConfig } from "./utils";
-import type {
-  AppHostingOptions,
-  AppHostingOutputBundleConfig,
-  FirebaseFunctionsOptions,
-} from "./types";
+import type { AppHostingOutputBundleConfig } from "./types";
 
 export type { FirebaseOptions as PresetOptions } from "./types";
 
@@ -27,7 +20,7 @@ const firebaseAppHosting = defineNitroPreset(
             version: "v1",
             runConfig: {
               runCommand: `node ${relative(nitro.options.rootDir, serverEntry)}`,
-              ...(nitro.options.firebase as AppHostingOptions)?.appHosting,
+              ...nitro.options.firebase?.appHosting,
             },
             metadata: {
               framework: nitro.options.framework.name || "nitro",
@@ -55,56 +48,4 @@ const firebaseAppHosting = defineNitroPreset(
   }
 );
 
-const firebase = defineNitroPreset(
-  {
-    entry: `./runtime/firebase`,
-    commands: {
-      deploy: "npx firebase-tools deploy",
-    },
-    firebase: {},
-    hooks: {
-      async compiled(nitro) {
-        await writeFirebaseConfig(nitro);
-        await updatePackageJSON(nitro);
-      },
-      "rollup:before": (nitro, rollupConfig) => {
-        // TODO: add options support back using virtual template
-        // nitro.options.appConfig.nitro = nitro.options.appConfig.nitro || {};
-        // nitro.options.appConfig.nitro.firebase = nitro.options.firebase;
-
-        const { serverFunctionName } = nitro.options
-          .firebase as FirebaseFunctionsOptions;
-        if (
-          serverFunctionName &&
-          serverFunctionName !== genSafeVariableName(serverFunctionName)
-        ) {
-          throw new Error(
-            `\`firebase.serverFunctionName\` must be a valid JS variable name: \`${serverFunctionName}\``
-          );
-        }
-
-        // Replace __firebaseServerFunctionName__ to actual name in entries
-        (rollupConfig.plugins as Plugin[]).unshift({
-          name: "nitro:firebase",
-          transform: (code, id) => {
-            if (basename(id).startsWith("firebase-gen-")) {
-              return {
-                code: code.replace(
-                  /__firebaseServerFunctionName__/g,
-                  serverFunctionName || "server"
-                ),
-                map: null,
-              };
-            }
-          },
-        } satisfies Plugin);
-      },
-    },
-  },
-  {
-    name: "firebase" as const,
-    url: import.meta.url,
-  }
-);
-
-export default [firebase, firebaseAppHosting] as const;
+export default [firebaseAppHosting] as const;
