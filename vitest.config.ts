9f0c8e3cc244a938fa740f3c2a986153bc281366
Kanon <44870505+ysknsid25@users.noreply.github.com>
2025-04-04 17:16:36 +0900

    test: only include src for coverage report (#3281)

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     testTimeout: 30_000,
     coverage: {
       reporter: ["text", "clover", "json"],
+      include: ["src/**/*.ts"],
     },
     include: ["test/**/*.test.ts"],
   },
