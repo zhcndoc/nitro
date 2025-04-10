55ebc8f7580e2c84feff29153d36d8ee3afe1a47
Pooya Parsa <pyapar@gmail.com>
2025-04-10 12:47:53 +0200

    refactor(dev): update serve-static to v2 (#3302)

--- src/dev/server.ts
+++ src/dev/server.ts
@@ -218,7 +218,14 @@ class DevServer {
         this.nitro.options.runtimeConfig.app.baseURL,
         asset.baseURL || "/"
       );
-      app.use(url, fromNodeMiddleware(serveStatic(asset.dir)));
+      app.use(
+        url,
+        fromNodeMiddleware(
+          serveStatic(asset.dir, {
+            dotfiles: "allow",
+          })
+        )
+      );
       if (!asset.fallthrough) {
         app.use(url, fromNodeMiddleware(servePlaceholder()));
       }
