32e1850760fa19466ab69d31636ead05a856dc6c
Kricsleo <32707098+kricsleo@users.noreply.github.com>
2025-05-13 18:52:17 +0800

    refactor(prerender): show details for 5xx handled errors (#3348)

--- src/presets/_nitro/runtime/nitro-prerenderer.ts
+++ src/presets/_nitro/runtime/nitro-prerenderer.ts
@@ -1,8 +1,27 @@
 import "#nitro-internal-pollyfills";
+import consola from "consola";
+import { getRequestHeader, getRequestURL, H3Error, isEvent } from "h3";
 import { useNitroApp } from "nitro/runtime";
 import { trapUnhandledNodeErrors } from "nitro/runtime/internal";
 
 const nitroApp = useNitroApp();
+
+nitroApp.hooks.hook("error", (error, context) => {
+  if (
+    isEvent(context.event) &&
+    !(error as H3Error).unhandled &&
+    (error as H3Error).statusCode >= 500 &&
+    getRequestHeader(context.event, "x-nitro-prerender")
+  ) {
+    const url = getRequestURL(context.event).href;
+    consola.error(
+      `[prerender error]`,
+      `[${context.event.method}]`,
+      `[${url}]`,
+      error
+    );
+  }
+});
 
 export const localFetch = nitroApp.localFetch;
 export const closePrerenderer = () => nitroApp.hooks.callHook("close");
