0c78d3cb206350fb19d75d814e606774f2d501ab
Pooya Parsa <pyapar@gmail.com>
2025-07-14 17:11:35 +0200

    feat(vercel): generate o11y routes (#3474)

--- test/presets/vercel.test.ts
+++ test/presets/vercel.test.ts
@@ -149,6 +149,294 @@ describe("nitro:preset:vercel", async () => {
                 "src": "(?<url>/rules/swr-ttl/.*)",
               },
               {
+                "dest": "/wasm/static-import",
+                "src": "/wasm/static-import",
+              },
+              {
+                "dest": "/wasm/dynamic-import",
+                "src": "/wasm/dynamic-import",
+              },
+              {
+                "dest": "/wait-until",
+                "src": "/wait-until",
+              },
+              {
+                "dest": "/stream",
+                "src": "/stream",
+              },
+              {
+                "dest": "/static-flags",
+                "src": "/static-flags",
+              },
+              {
+                "dest": "/route-group",
+                "src": "/route-group",
+              },
+              {
+                "dest": "/raw",
+                "src": "/raw",
+              },
+              {
+                "dest": "/prerender-custom.html",
+                "src": "/prerender-custom.html",
+              },
+              {
+                "dest": "/prerender",
+                "src": "/prerender",
+              },
+              {
+                "dest": "/node-compat",
+                "src": "/node-compat",
+              },
+              {
+                "dest": "/modules",
+                "src": "/modules",
+              },
+              {
+                "dest": "/jsx",
+                "src": "/jsx",
+              },
+              {
+                "dest": "/json-string",
+                "src": "/json-string",
+              },
+              {
+                "dest": "/imports",
+                "src": "/imports",
+              },
+              {
+                "dest": "/icon.png",
+                "src": "/icon.png",
+              },
+              {
+                "dest": "/file",
+                "src": "/file",
+              },
+              {
+                "dest": "/fetch",
+                "src": "/fetch",
+              },
+              {
+                "dest": "/error-stack",
+                "src": "/error-stack",
+              },
+              {
+                "dest": "/env",
+                "src": "/env",
+              },
+              {
+                "dest": "/context",
+                "src": "/context",
+              },
+              {
+                "dest": "/config",
+                "src": "/config",
+              },
+              {
+                "dest": "/assets/md",
+                "src": "/assets/md",
+              },
+              {
+                "dest": "/assets/all",
+                "src": "/assets/all",
+              },
+              {
+                "dest": "/api/upload",
+                "src": "/api/upload",
+              },
+              {
+                "dest": "/api/typed/user/john/post/coffee",
+                "src": "/api/typed/user/john/post/coffee",
+              },
+              {
+                "dest": "/api/typed/user/john",
+                "src": "/api/typed/user/john",
+              },
+              {
+                "dest": "/api/storage/item",
+                "src": "/api/storage/item",
+              },
+              {
+                "dest": "/api/storage/dev",
+                "src": "/api/storage/dev",
+              },
+              {
+                "dest": "/api/serialized/void",
+                "src": "/api/serialized/void",
+              },
+              {
+                "dest": "/api/serialized/tuple",
+                "src": "/api/serialized/tuple",
+              },
+              {
+                "dest": "/api/serialized/set",
+                "src": "/api/serialized/set",
+              },
+              {
+                "dest": "/api/serialized/null",
+                "src": "/api/serialized/null",
+              },
+              {
+                "dest": "/api/serialized/map",
+                "src": "/api/serialized/map",
+              },
+              {
+                "dest": "/api/serialized/function",
+                "src": "/api/serialized/function",
+              },
+              {
+                "dest": "/api/serialized/error",
+                "src": "/api/serialized/error",
+              },
+              {
+                "dest": "/api/serialized/date",
+                "src": "/api/serialized/date",
+              },
+              {
+                "dest": "/api/methods/get",
+                "src": "/api/methods/get",
+              },
+              {
+                "dest": "/api/methods/foo.get",
+                "src": "/api/methods/foo.get",
+              },
+              {
+                "dest": "/api/methods/default",
+                "src": "/api/methods/default",
+              },
+              {
+                "dest": "/api/methods",
+                "src": "/api/methods",
+              },
+              {
+                "dest": "/api/meta/test",
+                "src": "/api/meta/test",
+              },
+              {
+                "dest": "/api/kebab",
+                "src": "/api/kebab",
+              },
+              {
+                "dest": "/api/import-meta",
+                "src": "/api/import-meta",
+              },
+              {
+                "dest": "/api/hey",
+                "src": "/api/hey",
+              },
+              {
+                "dest": "/api/hello2",
+                "src": "/api/hello2",
+              },
+              {
+                "dest": "/api/hello",
+                "src": "/api/hello",
+              },
+              {
+                "dest": "/api/headers",
+                "src": "/api/headers",
+              },
+              {
+                "dest": "/api/errors",
+                "src": "/api/errors",
+              },
+              {
+                "dest": "/api/error",
+                "src": "/api/error",
+              },
+              {
+                "dest": "/api/echo",
+                "src": "/api/echo",
+              },
+              {
+                "dest": "/api/db",
+                "src": "/api/db",
+              },
+              {
+                "dest": "/api/cached",
+                "src": "/api/cached",
+              },
+              {
+                "dest": "/500",
+                "src": "/500",
+              },
+              {
+                "dest": "/_swagger",
+                "src": "/_swagger",
+              },
+              {
+                "dest": "/_scalar",
+                "src": "/_scalar",
+              },
+              {
+                "dest": "/_openapi.json",
+                "src": "/_openapi.json",
+              },
+              {
+                "dest": "/assets/[id]",
+                "src": "/assets/(?<id>[^/]+)",
+              },
+              {
+                "dest": "/api/typed/user/john/post/[postId]",
+                "src": "/api/typed/user/john/post/(?<postId>[^/]+)",
+              },
+              {
+                "dest": "/api/typed/user/john/[johnExtends]",
+                "src": "/api/typed/user/john/(?<johnExtends>[^/]+)",
+              },
+              {
+                "dest": "/api/typed/user/[userId]/post/firstPost",
+                "src": "/api/typed/user/(?<userId>[^/]+)/post/firstPost",
+              },
+              {
+                "dest": "/api/typed/user/[userId]/post/[postId]",
+                "src": "/api/typed/user/(?<userId>[^/]+)/post/(?<postId>[^/]+)",
+              },
+              {
+                "dest": "/api/typed/user/[userId]/[userExtends]",
+                "src": "/api/typed/user/(?<userId>[^/]+)/(?<userExtends>[^/]+)",
+              },
+              {
+                "dest": "/api/typed/user/[userId]",
+                "src": "/api/typed/user/(?<userId>[^/]+)",
+              },
+              {
+                "dest": "/api/test/[-]/foo",
+                "src": "/api/test/(?<_0>[^/]*)/foo",
+              },
+              {
+                "dest": "/api/param/[test-id]",
+                "src": "/api/param/(?<test>[^/]+)-id",
+              },
+              {
+                "dest": "/tasks/[...name]",
+                "src": "/tasks/?(?<name>.+)",
+              },
+              {
+                "dest": "/rules/[...slug]",
+                "src": "/rules/?(?<slug>.+)",
+              },
+              {
+                "dest": "/api/wildcard/[...param]",
+                "src": "/api/wildcard/?(?<param>.+)",
+              },
+              {
+                "dest": "/api/typed/todos/[...]",
+                "src": "/api/typed/todos/?(?<_>.*)",
+              },
+              {
+                "dest": "/api/typed/todos/[todoId]/comments/[...commentId]",
+                "src": "/api/typed/todos/(?<todoId>[^/]+)/comments/?(?<commentId>.+)",
+              },
+              {
+                "dest": "/api/typed/catchall/some/[...test]",
+                "src": "/api/typed/catchall/some/?(?<test>.+)",
+              },
+              {
+                "dest": "/api/typed/catchall/[slug]/[...another]",
+                "src": "/api/typed/catchall/(?<slug>[^/]+)/?(?<another>.+)",
+              },
+              {
                 "dest": "/__nitro",
                 "src": "/(.*)",
               },
