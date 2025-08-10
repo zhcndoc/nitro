e85665a09b41b742b08b6e3b42f765f6e9db1e20
Manuel Schiller <manuel.schiller@caligano.de>
2025-08-10 18:42:14 +0200

    fix(vite): pass on request props in dispatcher (#3531)

--- src/runtime/internal/vite/dispatcher.mjs
+++ src/runtime/internal/vite/dispatcher.mjs
@@ -8,5 +8,17 @@ export default defineHandler(async (event) => {
   }
   return fetch(new URL(`/${m.params?._ || ""}${event.url.search}`, event.url), {
     viteEnv: m.data.service,
+    method: event.req.method,
+    // TODO: Clone headers or full RequestInit
+    headers: event.req.headers,
+    body: event.req.body,
+    credentials: event.req.credentials,
+    keepalive: event.req.keepalive,
+    cache: event.req.cache,
+    redirect: event.req.redirect,
+    referrer: event.req.referrer,
+    referrerPolicy: event.req.referrerPolicy,
+    integrity: event.req.integrity,
+    mode: event.req.mode,
   });
 });
