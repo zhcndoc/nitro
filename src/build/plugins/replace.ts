bc647b03e4bc10f988b2b10554f7c59c0abba0b8
Pooya Parsa <pooya@pi0.io>
2025-08-21 23:32:02 +0200

    fix(rolldown): alternative regex for raw skip

--- src/build/plugins/replace.ts
+++ src/build/plugins/replace.ts
@@ -2,7 +2,7 @@ import _replace from "@rollup/plugin-replace";
 import type { RollupReplaceOptions } from "@rollup/plugin-replace";
 import type { Plugin } from "rollup";
 
-const NO_REPLACE_RE = /ROLLUP_NO_REPLACE/;
+const NO_REPLACE_RE = /ROLLUP_NO_REPLACE|\\0raw:/;
 
 export function replace(options: RollupReplaceOptions): Plugin {
   const _plugin = _replace(options);
@@ -11,6 +11,7 @@ export function replace(options: RollupReplaceOptions): Plugin {
     // https://github.com/rollup/plugins/blob/master/packages/replace/src/index.js#L94
     renderChunk(code, chunk, options) {
       if (!NO_REPLACE_RE.test(code)) {
+        console.log("Transforming chunk:", chunk.fileName);
         // prettier-ignore
         // @ts-ignore
         return (_plugin.renderChunk as () => any).call(this, code, chunk, options );
