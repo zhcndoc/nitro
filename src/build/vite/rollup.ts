zhcndoc/nitro

commit 9ca983457bb84354e21c6144f53d330ef14d6155

fix(vite): enable `virtualBundle` behind an experimental flag (#3603)

src/build/vite/rollup.ts
@@ -52,7 +52,8 @@ export const getViteRollupConfig = (
     input: nitro.options.entry,
     external: [...base.env.external],
     plugins: [
-      virtualBundlePlugin(ctx._serviceBundles),
+      ctx.pluginConfig.experimental?.virtualBundle &&
+        virtualBundlePlugin(ctx._serviceBundles),
       ...baseBuildPlugins(nitro, base),
       alias({ entries: base.aliases }),
       replace({ preventAssignment: true, values: base.replacements }),

src/build/vite/plugin.ts
@@ -233,9 +233,10 @@ function mainPlugin(ctx: NitroPluginContext): VitePlugin[] {
 
         const services = ctx.pluginConfig.services || {};
         const serviceNames = Object.keys(services);
-        if (serviceNames.includes(name)) {
-          // we don't write to the file system
-          // instead, the generateBundle hook will capture the output and write it to the virtual file system to be used by the nitro build later
+        if (
+          serviceNames.includes(name) &&
+          ctx.pluginConfig.experimental?.virtualBundle
+        ) {
           config.build ??= {};
           config.build.write = config.build.write ?? false;
         }

src/build/vite/prod.ts
@@ -1,7 +1,7 @@
 import type { ViteBuilder } from "vite";
 import type { NitroPluginContext } from "./types";
 
-import { relative } from "pathe";
+import { relative, resolve } from "pathe";
 import { formatCompatibilityDate } from "compatx";
 import { copyPublicAssets, prerender } from "../..";
 import { nitroServerName } from "../../utils/nitro";
@@ -87,7 +87,20 @@ export function prodEntry(ctx: NitroPluginContext): string {
   const serviceNames = Object.keys(services);
   const result = [
     // Fetchable services
-    `const services = { ${serviceNames.map((name) => `[${JSON.stringify(name)}]: () => import("${ctx._entryPoints[name]}")`)}};`,
+    `const services = { ${serviceNames.map((name) => {
+      let entry: string;
+      if (ctx.pluginConfig.experimental?.virtualBundle) {
+        entry = ctx._entryPoints[name];
+      } else {
+        entry = resolve(
+          ctx.nitro!.options.buildDir,
+          "vite/services",
+          name,
+          ctx._entryPoints[name]
+        );
+      }
+      return `[${JSON.stringify(name)}]: () => import("${entry}")`;
+    })}};`,
     /* js */ `
               const serviceHandlers = {};
               const originalFetch = globalThis.fetch;

src/build/vite/types.ts
@@ -17,6 +17,14 @@ export interface NitroPluginConfig {
    * @internal Pre-initialized Nitro instance.
    */
   _nitro?: Nitro;
+
+  experimental?: {
+    /**
+     * @experimental Use the virtual filesystem for intermediate environment build output files.
+     * @note This is unsafe if plugins rely on temporary files on the filesystem.
+     */
+    virtualBundle?: boolean;
+  };
 }
 
 export interface ServiceConfig {

commit 021572528988f5f171a3e4f912b2adf0cfb59f81

fix(vite): filter falsy plugins
(regression from #3603)

src/build/vite/rollup.ts
@@ -58,7 +58,7 @@ export const getViteRollupConfig = (
       alias({ entries: base.aliases }),
       replace({ preventAssignment: true, values: base.replacements }),
       inject(base.env.inject),
-    ],
+    ].filter(Boolean) as RollupPlugin[],
     treeshake: {
       moduleSideEffects(id) {
         const normalizedId = normalize(id);