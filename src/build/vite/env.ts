9b276e11d3a4e275ae1eca765d1621c85d9b2c0a
Pooya Parsa <pooya@pi0.io>
2025-12-09 11:58:14 +0100

    fix(vite): temp fix for react bundling
    mainly fixing tanstack-start with rolldown-vite...

--- src/build/vite/env.ts
+++ src/build/vite/env.ts
@@ -70,6 +70,7 @@ export function createServiceEnvironment(
       emptyOutDir: true,
     },
     resolve: {
+      noExternal: ctx.nitro!.options.dev ? undefined : [/react/],
       conditions: ctx.nitro!.options.exportConditions,
       externalConditions: ctx.nitro!.options.exportConditions?.filter(
         (c) => !/browser|wasm/.test(c)
