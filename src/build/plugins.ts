a8c8cbaa316a9ba1784f5b9df12916a674b2175f
Pooya Parsa <pooya@pi0.io>
2026-01-22 20:55:20 +0100

    feat: add non bundlable dependencies from nf3 db

--- src/build/plugins.ts
+++ src/build/plugins.ts
@@ -11,10 +11,6 @@ import { virtual, virtualDeps } from "./plugins/virtual.ts";
 import { sourcemapMinify } from "./plugins/sourcemap-min.ts";
 import { raw } from "./plugins/raw.ts";
 import { externals } from "./plugins/externals.ts";
-import { NodeNativePackages } from "nf3";
-
-// Additional dependencies known to have bundling issues
-const FORCE_TRACE_DEPS = ["pg"];
 
 export async function baseBuildPlugins(nitro: Nitro, base: BaseBuildConfig) {
   const plugins: Plugin[] = [];
@@ -62,10 +58,12 @@ export async function baseBuildPlugins(nitro: Nitro, base: BaseBuildConfig) {
   if (nitro.options.node && nitro.options.noExternals !== true) {
     const isDevOrPrerender =
       nitro.options.dev || nitro.options.preset === "nitro-prerender";
+    const { NodeNativePackages, NonBundleablePackages } =
+      await import("nf3/db");
     const traceDeps = [
       ...new Set([
         ...NodeNativePackages,
-        ...FORCE_TRACE_DEPS,
+        ...NonBundleablePackages,
         ...(nitro.options.traceDeps || []),
       ]),
     ];
