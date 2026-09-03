5113c780810c8dede576a966bd43af7ccde00709
Rihan Arfan <me@file.properties>
2026-09-03 21:13:45 +0100

    presets(aws-lambda): switch to srvx (#4056)

--- src/presets/aws-lambda/runtime/aws-lambda.ts
+++ src/presets/aws-lambda/runtime/aws-lambda.ts
@@ -1,28 +1,12 @@
 import "#nitro/virtual/polyfills";
+import { handleLambdaEvent } from "srvx/aws-lambda";
 import { useNitroApp } from "nitro/app";
-import { awsRequest, awsResponseHeaders, awsResponseBody } from "./_utils.ts";
 
-import type {
-  APIGatewayProxyEvent,
-  APIGatewayProxyEventV2,
-  APIGatewayProxyResult,
-  APIGatewayProxyResultV2,
-  Context,
-} from "aws-lambda";
+import type { AwsLambdaEvent } from "srvx/aws-lambda";
+import type { Context } from "aws-lambda";
 
 const nitroApp = useNitroApp();
 
-export async function handler(
-  event: APIGatewayProxyEvent | APIGatewayProxyEventV2,
-  context: Context
-): Promise<APIGatewayProxyResult | APIGatewayProxyResultV2> {
-  const request = awsRequest(event, context);
-
-  const response = await nitroApp.fetch(request);
-
-  return {
-    statusCode: response.status,
-    ...awsResponseHeaders(response),
-    ...(await awsResponseBody(response)),
-  };
+export async function handler(event: AwsLambdaEvent, context: Context) {
+  return handleLambdaEvent(nitroApp.fetch, event, context);
 }
